ae50e54f87b66da66ec29a74d92a89433a3589e8
Samuel Debruyn <code.s@muel.be>
2015-09-03 19:00:14 +0200

    add support for pacman based distros

--- run.sh
+++ run.sh
@@ -11,42 +11,61 @@ command_exists()
     hash "$1" 2>/dev/null
 }
 
+SOURCES_UPDATED=false
+update_sources()
+{
+    if [ "$SOURCES_UPDATED" = false ] ; then
+        if command_exists apt-get; then
+            apt-get update
+        fi
+        if command_exists pacman; then
+            pacman -Syu
+        fi
+        SOURCES_UPDATED=true
+    fi 
+}
+
 install_hugo()
 {
     # check if curl is installed
     # install otherwise
     if ! command_exists curl; then
-        echo "curl not found, installing curl..."
+        update_sources
         if command_exists apt-get; then
-            apt-get update && apt-get install -y curl
+            apt-get install -y curl
+        elif command_exists pacman; then
+            pacman -S --noconfirm curl
         else
             yum install -y curl
         fi
     fi
     
-    cd ${WERCKER_STEP_ROOT}
-    echo "downloading hugo with curl..."
-    curl -sL https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_linux_amd64.tar.gz -o ${WERCKER_STEP_ROOT}/hugo_${HUGO_VERSION}_linux_amd64.tar.gz
-    echo "unpacking hugo..."
-    tar xzf hugo_${HUGO_VERSION}_linux_amd64.tar.gz
-    export HUGO_COMMAND=${WERCKER_STEP_ROOT}/hugo_${HUGO_VERSION}_linux_amd64/hugo_${HUGO_VERSION}_linux_amd64
-    echo "we will be using ${HUGO_COMMAND}"
+    cd $WERCKER_STEP_ROOT    
+    curl -sL https://github.com/spf13/hugo/releases/download/v${WERCKER_HUGO_BUILD_VERSION}/hugo_${WERCKER_HUGO_BUILD_VERSION}_linux_amd64.tar.gz -o ${WERCKER_STEP_ROOT}/hugo_${WERCKER_HUGO_BUILD_VERSION}_linux_amd64.tar.gz
+    tar xzf hugo_${WERCKER_HUGO_BUILD_VERSION}_linux_amd64.tar.gz
+    HUGO_COMMAND=${WERCKER_STEP_ROOT}/hugo_${WERCKER_HUGO_BUILD_VERSION}_linux_amd64/hugo_${WERCKER_HUGO_BUILD_VERSION}_linux_amd64
 }
 
-# check if git is installed
-if ! command_exists git; then
-    echo "git not found, installing git..."
-    if command_exists apt-get; then
-        apt-get update && apt-get install -y git
-    else
-        yum install -y git
+install_git()
+{
+    # check if pygments is installed
+    # install otherwise
+    if ! command_exists git; then
+        update_sources
+        if command_exists apt-get; then
+            apt-get install -y git
+        elif command_exists pacman; then
+            pacman -S --noconfirm git
+        else
+            yum install -y git
+        fi
     fi
-fi
+}
 
 # set the theme name
 if [ ! -n "$WERCKER_HUGO_THEME_CHECK_THEME" ]; then
     echo "no theme name set, using ${DEFAULT_THEME_NAME}"
-    export WERCKER_HUGO_THEME_CHECK_THEME=DEFAULT_THEME_NAME
+    WERCKER_HUGO_THEME_CHECK_THEME=DEFAULT_THEME_NAME
 fi
 
 #check if hugo is already installed in the container
@@ -56,8 +75,10 @@ if ! command_exists "hugo"; then
 else
     hugo_version_output=`hugo version`
     echo "using the installed ${hugo_version_output}..."
-    export HUGO_COMMAND="hugo"
+    HUGO_COMMAND="hugo"
 fi
+
+install_git
 
 # clone the example site
 cd ${WERCKER_STEP_ROOT}
